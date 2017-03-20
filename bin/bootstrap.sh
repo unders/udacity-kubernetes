2bfbd6dcc790440a65ae5a1700cf5209b1716a9d
Anders Törnqvist <anders.tornqvist@gmail.com>
2017-03-20 09:38:37 +0100

    python is required when installing SDK.

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
+command_exists () {
+    type "$1" &> /dev/null ;
+}
+
+python_is_required() {
+    if command_exists python; then
+        return
+    fi
+
+    echo "You must install: python 2.7"
+    exit 0
+}
+
 main() {
-    echo "Installing Cloud SDK"
+    python_is_required
+    echo "Installing Google Cloud SDK"
 }
 
 main
